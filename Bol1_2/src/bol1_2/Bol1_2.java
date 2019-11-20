45934bcd7db1ca6212b6be6b116f1aefcd22f7ad
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2019-11-20 09:03:45 +0100

    Bol1_2

---
+++ Bol1_2/src/bol1_2/Bol1_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol1_2;
+
+/**
+ *
+ * @author lromerofajar
+ */
+public class Bol1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**Realiza un ordinograma que permita calcular o área 
+        dun cadrado de 3m de lado.De seguido crea un 
+        proxecto en java, co nome boletin1_2 , para executalo*/
+        
+        float lado=3f, area;
+        area = lado*lado;
+        System.out.println("área = "+ lado*lado);
+    }
+    
+}
