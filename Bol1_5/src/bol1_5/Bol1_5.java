b13f49be86408ad38e712d2d79df043c3cb378ec
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2019-11-20 09:04:20 +0100

    Bol1_5

---
+++ Bol1_5/src/bol1_5/Bol1_5.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol1_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lromerofajar
+ */
+public class Bol1_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        final float MILLA = 1852f;
+        System.out.println("Escribe las millas");
+        Scanner metros= new Scanner (System.in);
+        float met = metros.nextFloat();
+        
+        
+        
+    }
+    
+}
